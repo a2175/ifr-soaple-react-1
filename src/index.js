1ae61ef96429a50f1cba0fe01732275417eb7729
a2175 <as217512@gmail.com>
2024-12-26 19:36:36 +0900

    (실습) Hooks 사용해보기

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import NotificationList from './chapter_06/NotificationList';
+import Accommodate from './chapter_07/Accommodate';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NotificationList />
+    <Accommodate />
   </React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
