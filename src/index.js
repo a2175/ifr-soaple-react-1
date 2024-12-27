a83aea6a54989e3d0615c2faa5ff7e890e1b8f76
a2175 <as217512@gmail.com>
2024-12-27 19:03:37 +0900

    (실습) 클릭 이벤트 처리하기

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import Accommodate from './chapter_07/Accommodate';
+import ConfirmButton from './chapter_08/ConfirmButton';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Accommodate />
+    <ConfirmButton />
   </React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
